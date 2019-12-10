74dfa93c036bdb90e105469751d55cbd80f6d937
Nils <spuravs85@gmail.com>
2019-12-10 08:57:48 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def index():
     return 'Hello world'
 
+@app.route('/russia')
+def index():
+    return 'Russia is big country,ruled by russians!'
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
